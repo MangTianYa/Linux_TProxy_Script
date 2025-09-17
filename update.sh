6248dd473f3c0507bace3c5b1ddf02d05219c036
YUeH885 <yuzu23234@gmail.com>
2025-09-17 20:52:21 +0800

    Unified the name

--- update.sh
+++ update.sh
@@ -3,12 +3,12 @@
 dir=$(cd "$(dirname "$0")" && pwd)
 
 # Download files
-wget -O "/tmp/china.txt" https://raw.githubusercontent.com/gaoyifan/china-operator-ip/refs/heads/ip-lists/china.txt || exit 1
-wget -O "/tmp/china6.txt" https://raw.githubusercontent.com/gaoyifan/china-operator-ip/refs/heads/ip-lists/china6.txt || exit 1
+wget -O "/tmp/cn_ipv4.txt" https://raw.githubusercontent.com/gaoyifan/china-operator-ip/refs/heads/ip-lists/china.txt || exit 1
+wget -O "/tmp/cn_ipv6.txt" https://raw.githubusercontent.com/gaoyifan/china-operator-ip/refs/heads/ip-lists/china6.txt || exit 1
 
 # Process rule files
-cp "/tmp/china.txt" "$dir/china.txt"
-cp "/tmp/china6.txt" "$dir/china6.txt"
+cp "/tmp/cn_ipv4.txt" "$dir/cn_ipv4.txt"
+cp "/tmp/cn_ipv6.txt" "$dir/cn_ipv6.txt"
 
 # readd cnip
 $dir/cnip.sh
